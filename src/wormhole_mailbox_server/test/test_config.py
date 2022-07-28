4d9da01aaf6211c6e7128f2fbfc4943e8cc0dbf3
meejah <meejah@meejah.ca>
2022-07-28 01:29:11 -0600

    some unit-tests

--- src/wormhole_mailbox_server/test/test_config.py
+++ src/wormhole_mailbox_server/test/test_config.py
@@ -19,6 +19,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_advertise_version(self):
@@ -35,6 +36,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_blur(self):
@@ -51,6 +53,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_channel_db(self):
@@ -67,6 +70,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_disallow_list(self):
@@ -83,6 +87,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_log_fd(self):
@@ -99,6 +104,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": 5,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_port(self):
@@ -115,6 +121,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
         o = server_tap.Options()
@@ -130,6 +137,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_signal_error(self):
@@ -146,6 +154,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_usage_db(self):
@@ -162,6 +171,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [],
+                             "permissions": "none",
                              })
 
     def test_websocket_protocol_option_1(self):
@@ -178,6 +188,7 @@ class Config(unittest.TestCase):
                              "motd": None,
                              "log-fd": None,
                              "websocket-protocol-options": [("foo", "bar")],
+                             "permissions": "none",
                              })
 
     def test_websocket_protocol_option_2(self):
@@ -198,6 +209,7 @@ class Config(unittest.TestCase):
                              "websocket-protocol-options": [("foo", "bar"),
                                                             ("baz", [1, "buz"]),
                                                             ],
+                             "permissions": "none",
                              })
 
     def test_websocket_protocol_option_errors(self):
